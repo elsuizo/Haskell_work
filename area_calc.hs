0c4f12571880cb9e91edb5f853cbcc3ecad3d96e
elsuizo <mnoblia@disroot.org>
2019-09-15 19:35:11 -0300

    hola mundo en Haskell!!!

---
+++ area_calc.hs
@@ -0,0 +1,2 @@
+-- Programa para calcular el area de un circulo
+area r = pi * r ^ 2 -- notemos que pi es una constante que ya esta definida en el lenguaje como palabra reservada
---
+++ hola_mundo.hs
@@ -0,0 +1,3 @@
+-- Mi primer programa en Haskell!!!
+mensaje = "hola mundo"
+main = putStrLn mensaje
