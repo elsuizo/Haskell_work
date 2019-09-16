521b37ccc7e37c359bf6063e7e0546332aabf08c
elsuizo <mnoblia@disroot.org>
2019-09-15 21:12:30 -0300

    mas ejemplos!! :beers:

---
+++ absolute.hs
@@ -0,0 +1,4 @@
+-- Funcion para calcular el valor absoluto de un numero (real)
+absolute x
+   | x < 0      = -x
+   | otherwise  = x
--- area_calc.hs
+++ area_calc.hs
@@ -1,2 +1,4 @@
 -- Programa para calcular el area de un circulo
 area r = pi * r ^ 2 -- notemos que pi es una constante que ya esta definida en el lenguaje como palabra reservada
+
+areaCilinder r h = area r * h
