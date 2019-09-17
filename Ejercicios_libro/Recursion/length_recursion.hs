2411f86629fa64b1351e762e7e5e59638be536a1
elsuizo <mnoblia@disroot.org>
2019-09-17 13:57:12 -0300

    update no se que...

--- Ejercicios_libro/Recursion/length_recursion.hs
+++ Ejercicios_libro/Recursion/length_recursion.hs
@@ -2,4 +2,6 @@
 Define length using an auxiliary function and an accumulating parameter,
 as in the loop-like alternate version of factorial
 -------------------------------------------------------------------------}
-
+length' :: [Integer] -> Int -- declaramos el type de nuestra funcion
+length' [] = 0 -- el length' de una lista vacia es cerooo!!!
+length' (xs:ys)
